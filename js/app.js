f870a940f77b30d5b4ca68747cadbeb79d12cf88
HectorHTL <113003064+HectorHTL@users.noreply.github.com>
2022-09-27 22:31:47 -0500

    feat: add functions

--- js/app.js
+++ js/app.js
@@ -2,31 +2,3 @@ if(navigator.serviceWorker){
     navigator.serviceWorker.register('/sw.js')
 }
 
-
-
-
-if(window.caches){
-    console.log('Soportas caches');
-
-    caches.open('cache-v1');
- 
-
-   
-
-
-    caches.open('cache-v1').then((cache)=>{
-  
-        cache.addAll(
-         [
-            '/',
-            '/index.html',
-            '/pages/suma.html',
-            '/pages/resta.html',
-            '/pages/multiplicacion.html',
-            '/pages/division.html'
-         ]
-        )
-
-        
-    })
-}
--- sw.js
+++ sw.js
@@ -1,3 +1,30 @@
-self.addEventListener('install',(event)=>{
-    console.log('SW: Instalado');
+self.addEventListener('install', (event)=>{
+    console.log('SW: Install'); 
+    const promiseCache = caches.open('Cache-v1').then((cache)=>{
+         return cache.addAll(
+             [
+                 '/',
+                 '/index.html',
+                 '/css/style.css',
+                 '/js/app.js',
+                 '/pages/suma.html',
+                 '/pages/division.html',
+                 '/pages/multiplicacion.html',
+                 '/pages/resta.html',
+                 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js',
+                 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css',
+             ]
+         );
+    });
+ 
+    event.waitUntil(promiseCache);
+ 
+ });
+ 
+
+
+ // Only Cache
+self.addEventListener('fetch', event =>{
+    const respCache = caches.match(event.request)
+    event.respondWith(respCache);
 });
